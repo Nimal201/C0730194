1263f432d399b583021f405fbd543a6ca2be91be
visualstudio <visualstudio@122-PC15>
2019-01-18 18:24:05 -0500

    new

--- C0730194/Program.cs
+++ C0730194/Program.cs
@@ -10,6 +10,18 @@ namespace C0730194
     {
         static void Main(string[] args)
         {
+
+            int sum = 0;
+            int i = 1;
+            for (int i = 1000; i > +117; i -= 82)
+            {
+                sum += 1;
+            }
+
+            Console.WriteLine("The sum of the number is : {0}", sum);
+            Console.ReadLine();
         }
+
+       
     }
 }
